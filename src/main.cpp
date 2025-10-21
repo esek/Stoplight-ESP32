53a2a9b069db3ddac908a0a7bd1949f53ddc7e03
SeanEvans235 <seanevans235@gmail.com>
2025-10-22 00:02:20 +0200

    the lights are on but no ones home

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
 
+#define HALL1 4
+#define HALL2 0
+
+#define REDLIGHT 12
+#define YELLOWLIGHT 14
+#define GREENLIGHT 27
+
 // put function declarations here:
-int myFunction(int, int);
+
+int hall1_reading;
+int hall2_reading;
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(HALL1, ANALOG);
+  pinMode(HALL2, ANALOG);
+
+  pinMode(REDLIGHT, OUTPUT);
+  pinMode(YELLOWLIGHT, OUTPUT);
+  pinMode(GREENLIGHT, OUTPUT);
+  Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  hall1_reading = analogRead(HALL1);
+  hall2_reading = analogRead(HALL2);
+  if((hall1_reading  > 200) && (hall2_reading > 200)){
+    digitalWrite(GREENLIGHT, HIGH);
+    digitalWrite(YELLOWLIGHT, LOW);
+    digitalWrite(REDLIGHT, LOW);
+  }
+  else if((hall1_reading  < 200) && (hall2_reading < 200)){
+    digitalWrite(GREENLIGHT, LOW);
+    digitalWrite(YELLOWLIGHT, LOW);
+    digitalWrite(REDLIGHT, HIGH);
+  }
+  else{
+    digitalWrite(GREENLIGHT, LOW);
+    digitalWrite(YELLOWLIGHT, HIGH);
+    digitalWrite(REDLIGHT, LOW);
+  }
+  delay(100);
+  Serial.printf("Hall1 %i \n", hall1_reading);
+  Serial.printf("Hall2 %i \n", hall2_reading);
+  
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
