331cd2eb97ab8294ff24b7ccd94106b007329f6c
SeanEvans235 <seanevans235@gmail.com>
2025-10-27 21:06:09 +0100

    wifi checking

--- src/wifitest.cpp
+++ src/wifitest.cpp
@@ -1,29 +1,54 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-#define WIFISSID "mywifi"
-#define WIFIPASSWORD "1234567890"
+#define WIFISSID "Esek-HK"
+#define WIFIPASSWORD "jag lovar att bete mig"
 
-bool isConnected = false;
+int numnetworks;
 
 void setup() {
     Serial.begin(9600);
     WiFi.begin(WIFISSID, WIFIPASSWORD);
     Serial.println("Start");
+    
 
 }
 
 
 
 void loop() {
-    if(WiFi.status() == WL_CONNECTED && !isConnected){
+    /*
+    numnetworks = WiFi.scanNetworks();
+    printf("Found %i Networks \n", numnetworks);
+    
+    for (int i = 0; i < numnetworks; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+    Serial.println("done! new scan soon\n\n");
+    delay(10000);*/
+
+    
+    
+    if(WiFi.status() == WL_CONNECTED){
         Serial.println("Connected");
         isConnected = true;
     }
-    if(WiFi.status() != WL_CONNECTED){
-        Serial.println(".");
+    else if(WiFi.status() != WL_CONNECTED){
+        Serial.println("not connected");
         isConnected = false;
-        delay(500);
     }
+    else if(WiFi.status() != WL_CONNECTED){
+        Serial.println("not connected");
+        isConnected = false;
+    }
+    
 
 }
